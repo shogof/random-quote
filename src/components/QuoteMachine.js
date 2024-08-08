8ce8ac043dd9511438a5d5c02ed257e1bf6c6571
Shegofa <shogofadeveloper12@gmail.com>
2024-08-08 14:43:38 +0400

    linters error solved

--- src/components/QuoteMachine.js
+++ src/components/QuoteMachine.js
@@ -1,17 +1,37 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
-import { FaCheck } from "react-icons/fa";
-import "./QuoteMachine.css";
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { FaCheck } from 'react-icons/fa';
+import './QuoteMachine.css';
 
 const QuoteMachine = () => {
-  const [quote, setQuote] = useState("");
-  const [author, setAuthor] = useState("");
-  const [bgImage, setBgImage] = useState("");
+  const [quote, setQuote] = useState('');
+  const [author, setAuthor] = useState('');
+  const [bgImage, setBgImage] = useState('');
   const [copied, setCopied] = useState(false);
 
-  const images = [
-    "bg1.jpg", "bg2.jpg", "bg3.jpg", "bg4.jpg"
-  ];
+  const fetchQuote = async () => {
+    try {
+      const response = await axios.get(
+        'https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json'
+      );
+      const quotesData = response.data;
+      const randomQuote =
+        quotesData.quotes[Math.floor(Math.random() * quotesData.quotes.length)];
+
+      setQuote(randomQuote.quote);
+      setAuthor(randomQuote.author);
+      setCopied(false);
+    } catch (error) {
+      console.error('Error fetching quotes:', error);
+    }
+  };
+
+  const setRandomBackgroundImage = () => {
+    const randomImage = images[Math.floor(Math.random() * images.length)];
+    setBgImage(`/images/${randomImage}`);
+  };
+
+  const images = ['bg1.jpg', 'bg2.jpg', 'bg3.jpg', 'bg4.jpg'];
 
   useEffect(() => {
     fetchQuote();
@@ -24,56 +44,50 @@ const QuoteMachine = () => {
     return () => clearInterval(intervalId);
   }, []);
 
-  const fetchQuote = async () => {
-    try {
-      const response = await axios.get(
-        "https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json"
-      );
-      const quotesData = response.data;
-      const randomQuote =
-        quotesData.quotes[Math.floor(Math.random() * quotesData.quotes.length)];
-
-      setQuote(randomQuote.quote);
-      setAuthor(randomQuote.author);
-      setCopied(false);
-    } catch (error) {
-      console.error("Error fetching quotes:", error);
-    }
-  };
-
-  const setRandomBackgroundImage = () => {
-    const randomImage = images[Math.floor(Math.random() * images.length)];
-    setBgImage(`/images/${randomImage}`);
-  };
-
   const copyQuoteToClipboard = () => {
-    const textToCopy = `"${quote}" — ${author}`;
-    navigator.clipboard.writeText(textToCopy)
+    const textToCopy = `'${quote}' — ${author}`;
+    navigator.clipboard
+      .writeText(textToCopy)
       .then(() => {
         setCopied(true);
         setTimeout(() => setCopied(false), 2000);
       })
-      .catch(err => {
+      .catch((err) => {
         console.error('Failed to copy quote:', err);
       });
   };
 
   return (
-    <div className="quote-machine" style={{ backgroundImage: `url(${bgImage})` }}>
-        <h1>Quote Machine</h1>
-      <div className="quote-box">
-        <div className="quote-text">
-          <p id="text">{quote}</p>
+    <div
+      className='quote-machine'
+      style={{ backgroundImage: `url(${bgImage})` }}
+    >
+      <h1>Quote Machine</h1>
+      <div className='quote-box'>
+        <div className='quote-text'>
+          <p id='text'>{quote}</p>
         </div>
-        <div className="quote-author">
-          <p>- <span id="author">{author}</span></p>
+        <div className='quote-author'>
+          <p>
+            -<span id='author'>{author}</span>
+          </p>
         </div>
-        <div className="button-container">
-          <button className="button" id="new-quote" onClick={fetchQuote}>
+        <div className='button-container'>
+          <button
+            type='button'
+            className='button'
+            id='new-quote'
+            onClick={fetchQuote}
+          >
             New quote
           </button>
-          <button className="button" id="copy-quote" onClick={copyQuoteToClipboard}>
-            {copied ? <FaCheck className="check-icon" /> : "Copy Quote"}
+          <button
+            className='button'
+            type='button'
+            id='copy-quote'
+            onClick={copyQuoteToClipboard}
+          >
+            {copied ? <FaCheck className='check-icon' /> : 'Copy Quote'}
           </button>
         </div>
       </div>
