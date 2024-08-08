8f67cea3823bf2d8c18be91c7466da4178a911d6
Shegofa <shogofadeveloper12@gmail.com>
2024-08-08 11:37:24 +0400

    App.js added and QuouteMachine.js imported

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+// import QuoteMachine from './components/QuoteMachine';
+
+function App() {
+  return (
+    <div className="App">
+      <QuoteMachine />
+    </div>
+  );
+}
+
+export default App;
